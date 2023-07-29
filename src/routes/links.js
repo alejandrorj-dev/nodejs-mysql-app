24b684b76c16af387e8b98b3685e029104e19467
Alejandro Rodriguez <alejandrorodriguezjimenez17@outlook.com>
2023-07-29 13:43:13 -0500

    Deleting links created

---
+++ src/routes/links.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+const pool = require('../database');
+
+router.get('/add', (req, res) => {
+    res.render('links/add')
+});
+
+router.post('/add', async (req, res) => {
+    const { title, url, description } = req.body;
+    const newLink = {
+        title,
+        url,
+        description
+    };
+    await pool.query('INSERT INTO links set ?', [newLink]);
+    res.redirect('/links');
+});
+
+router.get('/', async (req, res) => {
+    const links = await pool.query('SELECT * FROM links');
+    res.render('links/links.hbs', { links: links });
+});
+
+router.get('/delete/:id', async (req, res) => {
+    const { id } = req.params;
+    pool.query('DELETE FROM links WHERE id = ?', [id]);
+    res.redirect('/links');
+});
+
+module.exports = router;
